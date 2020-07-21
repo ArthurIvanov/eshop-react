9f004d0a1181a6e2e0939f210136211742ff7400
Arthur Ivanov <arthur.ivanov.art@gmail.com>
2020-07-21 20:56:11 +0200

    final project version

---
+++ client/build/precache-manifest.89dde836ea96ea428a27d8792f3442d5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a8bf861a67128734acb50838b4aa4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1534ff86fbf66d666f3e",
+    "url": "/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "94c5147d919eade99128",
+    "url": "/static/js/2.e6e16a0a.chunk.js"
+  },
+  {
+    "revision": "104181c67a6c7168a03e",
+    "url": "/static/js/3.9ebc1564.chunk.js"
+  },
+  {
+    "revision": "afd9106e5e3949213fe4",
+    "url": "/static/js/4.118abdde.chunk.js"
+  },
+  {
+    "revision": "dab7f74064aa3ce1373c",
+    "url": "/static/js/5.fe8e87f8.chunk.js"
+  },
+  {
+    "revision": "da2c2aece0950aa2c2d0",
+    "url": "/static/js/6.2547af27.chunk.js"
+  },
+  {
+    "revision": "c0a762d512a99cb36360",
+    "url": "/static/js/7.1c3b8743.chunk.js"
+  },
+  {
+    "revision": "d146f81c6bf8a4704a3a",
+    "url": "/static/js/8.189f1d3d.chunk.js"
+  },
+  {
+    "revision": "1534ff86fbf66d666f3e",
+    "url": "/static/js/main.c83fd6b4.chunk.js"
+  },
+  {
+    "revision": "51599e1e5dfc61ad3284",
+    "url": "/static/js/runtime~main.9bbcd46c.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
